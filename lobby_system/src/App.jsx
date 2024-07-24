6f87c486592beac2bd65919b4c950207bb08cc26
Vgangaswamy <vgangaswamy@hawk.iit.edu>
2024-07-23 21:52:55 -0500

    first day

--- lobby_system/src/App.jsx
+++ lobby_system/src/App.jsx
@@ -4,14 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const TIC_TAC_TOE = new Array (3).fill(newArray(3).fill(null)) //[[null, null, null], [null, null, null], [null, null, null]]
+  const TIC_TAC_TOE = new Array (3).fill(new Array(3).fill(null)) //[[null, null, null], [null, null, null], [null, null, null]]
 
   return <div> 
       <h1>Tic Tac Toe</h1>
       {TIC_TAC_TOE.map(
       (row, i)=>{
         return row.map((cell, j)=>{
-          return <div>{i}-{j}</div>
+          return <div className='cell'>{i}-{j}</div>
 
         })
       }

c7ebb2cbcb1b24a2f83f3f5ac04335e2459a7286
Vgangaswamy <vgangaswamy@hawk.iit.edu>
2024-07-23 19:51:17 -0500

    first day

---
+++ lobby_system/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const TIC_TAC_TOE = new Array (3).fill(newArray(3).fill(null)) //[[null, null, null], [null, null, null], [null, null, null]]
+
+  return <div> 
+      <h1>Tic Tac Toe</h1>
+      {TIC_TAC_TOE.map(
+      (row, i)=>{
+        return row.map((cell, j)=>{
+          return <div>{i}-{j}</div>
+
+        })
+      }
+    )}
+    </div>
+}
+
+export default App
